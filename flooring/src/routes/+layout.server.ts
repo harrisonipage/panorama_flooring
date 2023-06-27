3f1dd055c1f0f8fd2ef1a1d706c29ef31b5fd1c4
h <harrisonpagews@gmail.com>
2023-06-27 19:05:22 -0400

    styling

--- flooring/src/routes/+layout.server.ts
+++ flooring/src/routes/+layout.server.ts
@@ -1,5 +1,4 @@
 export function load ({url}){
-    console.log(url)
     return{
         url: url.pathname,
     }
--- flooring/src/routes/about/+page.svelte
+++ flooring/src/routes/about/+page.svelte
@@ -1,12 +1,12 @@
 <div class="w-full h-fit flex flex-col justify-center bg-slate-100 text-black place-items-center">
     <div class="h-[4.5rem]"></div>
-    <div class="w-full md:h-96 h-44  bg-[url('/bg1.jpg')] bg-cover justify-center place-items-center flex">
-        <h1 class="text-6xl text-black">About Us</h1>
+    <div class="w-full md:h-96 h-44  bg-[url('/bg1.jpg')] bg-cover justify-center place-items-center flex mb-16 md:mb-24">
+        <h1 class="text-4xl text-black font-bold">About Us</h1>
     </div>
-    <div class="w-screen max-w-screen-2xl min-h-screen max-h-fit flex flex-col md:flex-row p-10 ">
-        <div class="w-full h-full flex flex-col gap-20 justify-center">
-            <h1 class="text-3xl">About Us</h1>
-            <div class="flex flex-col gap-5 leading-relaxed text-lg">
+    <div class="w-full max-w-screen-2xl min-h-screen max-h-fit grid md:grid-cols-2 grid-cols-1 p-7 md:p-12 gap-16">
+        <div class="w-full h-full flex flex-col gap-16 justify-start">
+            <h1 class="text-3xl">The right team for the job.</h1>
+            <div class="flex flex-col gap-5 leading-tight md:leading-relaxed text-lg">
                 <p>Panorama Flooring has been providing superior commercial flooring solutions to countless of Melbourne’s high-rise buildings, apartment blocks, hotels, office buildings and warehouses.</p>
                 <p>As a trusted and established industry leader, we offer a comprehensive range of flooring solutions and services designed to meet the unique needs of our clients.</p>
                 <p>Our team of experts is dedicated to hear out your needs and consult you to find the perfect flooring service for your Job</p>
@@ -14,10 +14,8 @@
                 <p>Not yet convinced? Check out our <a href="/projects"><span class="font-bold">projects</span></a> section and see our previously completed Projects.</p>
             </div>
         </div>
-        <div class="w-full h-full justify-end flex">
-            <div class="w-3/5 h-full bg-pink-200">
-
-            </div>
+        <div class="w-full h-[calc(100vh-4.5rem)] flex justify-end">
+            <div class=" bg-[url('/about.jpg')] bg-cover md:w-2/3 md:h-[calc(100vh-4.5rem)] h-[calc(100vh-8rem)] w-full"></div>
 
         </div>
 
