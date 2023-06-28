51d8a2c8bec3acbffdfbda646c5e78605607d7ba
h <harrisonpagews@gmail.com>
2023-06-28 13:16:06 -0400

    finished proposal

--- flooring/src/routes/projects/+page.svelte
+++ flooring/src/routes/projects/+page.svelte
@@ -4,16 +4,19 @@
     <div class="w-full md:h-96 h-44  bg-[url('/project.jpg')] bg-cover justify-center place-items-center flex">
         <h1 class="text-4xl text-black">Projects</h1>
     </div>
-    <div class="w-full max-w-screen-2xl min-h-screen max-h-fit flex flex-col p-5 md:p-10 ">
+    <div class="w-full max-w-screen-2xl min-h-screen max-h-fit flex flex-col p-5 md:p-10 gap-10">
         <div class="h-screen w-full flex flex-col gap-10">
             <h1 class="text-xl">Premier Tower <span class="italic text-slate-500">Spencer St, Melbourne VIC 3000</span></h1>
-            <div class="h-3/4 w-full bg-[url('/test.png')] bg-center bg-cover">
+            <div class="h-3/4 w-full bg-[url('https://www.premiertower.com.au/resources/images/home/main_tower_1.jpg')]  bg-center bg-cover">
             </div>
+            <a href="/projects/premier-tower" class="text-base px-10 btn btn-sm  rounded-none place-self-start bg-slate-300 ">See More</a>
         </div>
         <div class="h-screen w-full flex flex-col gap-10">
             <h1 class="text-xl">West Side Place <span class="italic text-slate-500">Spencer St, Melbourne VIC 3000</span></h1>
-            <div class="h-3/4 w-full bg-slate-200">
+            <div class="h-3/4 w-full bg-[url('https://westsideplace.com.au/wp-content/themes/wsp/dist/assets/images/apartments/WSP_view-01.jpg')] bg-cover">
             </div>
+            <a href="/projects/west-side-place" class="text-base px-10 btn btn-sm  rounded-none place-self-start bg-slate-300 ">See More</a>
+
         </div>
         <div class="h-screen w-full flex flex-col gap-10">
             <h1 class="text-xl"> Shangri La <span class="italic text-slate-500">Exhibition St, Melbourne VIC 3000</span></h1>
---
+++ flooring/src/routes/projects/west-side-place/+page.svelte
@@ -0,0 +1,14 @@
+<div class="w-full h-fit flex justify-center flex-col gap-20 bg-slate-200">
+    <div class="w-full h-screen  bg-[url('https://westsideplace.com.au/wp-content/themes/wsp/dist/assets/images/apartments/WSP_view-01.jpg')] bg-contain bg-center md:bg-cover  justify-center place-items-center flex">
+    </div>
+    <div class="w-full max-w-screen-2xl flex flex-col p-6 md:p-10 gap-10">
+        <h1 class="text-xl">West Side Place <span class="italic text-slate-500">Spencer St, Melbourne VIC 3000</span></h1>
+        <p>Acting as a subcontractor to Embelton, one of Victorias biggest flooring companies, Panorama Flooring helped complete the floor preparation for Timber, Vinyl and Carpet.</p>
+        <img src="https://cdn.shopify.com/s/files/1/0752/6419/6892/files/WSP_1_afd323bb-43f1-453c-b4f7-2b66fa1800af_600x600.png?v=1687117363" alt = "test"/>
+        <img src="https://cdn.shopify.com/s/files/1/0752/6419/6892/files/WSP_2_376145ec-4e34-425d-8495-888fc351df45_600x600.png?v=1687117384" alt = "test"/>
+        <img src="https://cdn.shopify.com/s/files/1/0752/6419/6892/files/WSP_4_335d8aa7-394b-48a2-a791-00cdf7090461_600x600.png?v=1687117435" alt = "test"/>
+        <img src="https://cdn.shopify.com/s/files/1/0752/6419/6892/files/WSP_3_63f261f3-ecf4-40e0-82db-3d5d5c8b374d_600x600.png?v=1687117410" alt = "test"/>
+
+    </div>
+
+</div>
